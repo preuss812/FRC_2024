d4aea3e63858efd4a21520f35713b51296f3f37a
Team 812! <preuss@team812.com>
2022-02-10 17:12:11 -0800

    Added Arm subsystem

--- team812/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ team812/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,10 +5,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.Constants.CANConstants;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import frc.robot.Constants.DriveTrainConstants;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
 
 public class ArmSubsystem extends SubsystemBase {
+  private final WPI_TalonSRX m_arm = new WPI_TalonSRX(CANConstants.kArmMotor);
+
+
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    m_arm.configFactoryDefault();
+    m_arm.setNeutralMode(NeutralMode.Brake);
+    m_arm.configOpenloopRamp(DriveTrainConstants.kOpenLoopRampRate);
+    m_arm.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    m_arm.setSelectedSensorPosition(0.0);
+    m_arm.setInverted(false);
+    m_arm.setSensorPhase(false); //Attempts to make it positive
+  }
 
   @Override
   public void periodic() {
